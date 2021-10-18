8285731c6bde0ca2d8542464e96947e2fbc8b40a
LeonardoCeccarelli <leo.ceccarelli@tiscali.it>
2021-10-18 15:57:30 +0200

    add btn and event listener

--- ex_pali/pali.js
+++ ex_pali/pali.js
@@ -1,28 +1,35 @@
-// Chiedo all'utente di inserire una parola
-const parolaUtente = prompt("Inserisci una parola")
+// Individuo il bottone da dove far partire la funzione
+const btnPali = document.getElementById("btn_palindromo")
 
-// Creo una validazione che mi ferma il codice 
-// se l'utente ha inserito un numero
-if (!isNaN(parolaUtente)) {
-    alert("Parola non valida")
-} else {
-    // Dopo aver creato la funzione l'aggiungo inserendo come argomento
-    // la variabile ricatava dall'inserimento della parola da 
-    // parte dell'utente
-    changeWordsDirection(parolaUtente)
-}
+// Ricevo dall'utente il valore inserito
+const elementoUtente = document.querySelector("[name='words']")
 
+btnPali.addEventListener("click", function () {
+    changeWordsDirection(elementoUtente)
+})
+
+/* ************************
+FUNZIONE IN GRADO DI VERIFICARE SE UNA PAROLA/FRASE È PALINDROMA 
+************************ */
 function changeWordsDirection() {
-    // Inverto la parola inserita dall'utente utilizzando le funzioni 
-    // split, reverse e join
-    const parolaReverse = parolaUtente.split("").reverse().join("")
 
-    // Creo la condizione che mi permette di mandare un messaggio
-    // all'utente che conferma se la parola è palindroma oppure no
-    if (parolaUtente.toLowerCase().replace(/\s+/g, '') === parolaReverse.toLowerCase().replace(/\s+/g, '')) {
-        alert(`La parola inserita è palindroma poichè "${parolaUtente}" se scritta al contrario non cambia (${parolaReverse}).`)
+    const parolaUtente = elementoUtente.value
+    // Creo una validazione che mi ferma il codice 
+    // se l'utente ha inserito un numero
+    if (!isNaN(parolaUtente)) {
+        alert("Parola non valida")
     } else {
-        alert(`La parola inserita (${parolaUtente}) non è palindroma.`)
+        // Inverto la parola inserita dall'utente utilizzando le funzioni 
+        // split, reverse e join
+        const parolaReverse = parolaUtente.split("").reverse().join("")
+
+        // Creo la condizione che mi permette di mandare un messaggio
+        // all'utente che conferma se la parola è palindroma oppure no
+        if (parolaUtente.toLowerCase().replace(/\s+/g, '') === parolaReverse.toLowerCase().replace(/\s+/g, '')) {
+            alert(`La parola inserita è palindroma poichè "${parolaUtente}" se scritta al contrario non cambia (${parolaReverse}).`)
+        } else {
+            alert(`La parola inserita (${parolaUtente}) non è palindroma.`)
+        }
     }
 }
 
